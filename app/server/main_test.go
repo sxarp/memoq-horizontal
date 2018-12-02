54dffb15aa8a30d75e16ca63ff4d59302ab887d7
sxarp <you@example.com>
2018-12-02 20:08:27 +0900

    add test

--- app/server/main_test.go
+++ app/server/main_test.go
@@ -1,9 +1,25 @@
 package main
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
 
-func TestServer(t *testing.T) {
-	if false {
-		t.Error("wrong!")
+func TestIndex(t *testing.T) {
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", index)
+
+	writer := httptest.NewRecorder()
+
+	request, _ := http.NewRequest("GET", "/", strings.NewReader(""))
+
+	mux.ServeHTTP(writer, request)
+
+	if code := writer.Code; code != 200 {
+		t.Errorf("response code = %v != 200", code)
 	}
 }
