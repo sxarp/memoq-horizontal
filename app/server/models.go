6bb77e945b863a1512b999c751d6649bf1e05bcb
sxarp <you@example.com>
2018-12-08 18:50:30 +0900

    fail test appropriately

--- app/server/models.go
+++ app/server/models.go
@@ -4,10 +4,50 @@ import (
 	"cloud.google.com/go/datastore"
 	"context"
 	"fmt"
+	"time"
 )
 
 type Entity struct {
 	Value string
+}
+
+func UpdateEntityProgressive(inputValue string) string {
+	project := "my-project-id"
+
+	keyToUpdate := "test_key"
+
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 10000*time.Millisecond)
+	defer cancel() // releases resources if slowOperation completes before timeout elapses
+
+	// Create a datastore client. In a typical application, you would create
+	// a single client which is reused for every datastore operation.
+	dsClient, err := datastore.NewClient(ctx, project)
+	if err != nil {
+		fmt.Printf("Failed to create DS client.")
+		fmt.Printf("%v", err)
+		panic("failed")
+	}
+
+	k := datastore.NameKey("Entity", keyToUpdate, nil)
+
+	eClear := &Entity{Value: ""}
+
+	if _, err := dsClient.Put(ctx, k, eClear); err != nil {
+		fmt.Printf("Failed to Put.")
+		fmt.Printf(err.Error())
+		panic("failed")
+	}
+
+	e := new(Entity)
+	if err := dsClient.Get(ctx, k, e); err != nil {
+		fmt.Printf("Failed to Get.")
+		panic("failed")
+	}
+
+	fmt.Printf("got value: %s", e.Value)
+
+	return inputValue
 }
 
 func UpdateEntity(inputValue string) string {
--- app/server/models_test.go
+++ app/server/models_test.go
@@ -7,7 +7,7 @@ import (
 func TestBasicDBOperaions(t *testing.T) {
 	inputValue := "hoge"
 
-	if res := UpdateEntity(inputValue); res != inputValue {
+	if res := UpdateEntityProgressive(inputValue); res != inputValue {
 		t.Errorf("Expected %s, got %s", inputValue, res)
 	}
 }
